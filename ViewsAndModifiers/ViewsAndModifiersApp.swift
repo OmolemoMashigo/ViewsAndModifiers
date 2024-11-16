c768883dfad869d33754e00a1b76ed5cc3b08eb8
Omolemo Mashigo <omomashigo@gmail.com>
2024-11-16 23:14:42 +0200

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Omolemo Mashigo on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
