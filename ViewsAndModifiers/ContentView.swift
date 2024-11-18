6e436f6e63d71af558ce7cb1d22d7a60fd53c541
Omolemo Mashigo <omomashigo@gmail.com>
2024-11-18 23:38:28 +0200

    simple conditional modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var useRedText = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Button("hello world"){
+            useRedText.toggle()
+        }.foregroundColor(useRedText ? .red : .blue)
     }
 }
 
